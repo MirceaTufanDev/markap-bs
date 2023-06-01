a76c7c4e4b47f2ad277c006bd95690be23935dce
RalucaMarianaStefan <mircea.tufan@yahoo.com>
2023-06-01 12:58:46 +0300

    First commit

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -25,7 +25,7 @@ import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import profilepic from '../images/profilepic.jpg';
+import profilepic from '../images/LogoImage.png';
 import testimonialImage from '../images/testimonial.webp';
 import {
   About,
@@ -69,28 +69,22 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `We are Markap Business Solutions`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        We are a Bucharest-based <strong className="text-stone-100">high-quality furniture factory</strong>, currently working with
+         <strong className="text-stone-100">commercial stores.</strong> We specialize in the production of modern, top-tier furniture designed to transform their business environments.
       </p>
+
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
-      </p>
+      When we're not crafting furniture, we engage in exploring the latest design trends,
+      <strong className="text-stone-100">perfecting our artisan skills</strong>,
+      and drawing inspiration from the diverse cultural heritage and architectural beauty of Bucharest, elements that consistently reflect in our unique furniture pieces.
+       </p>
     </>
   ),
   actions: [
-    {
-      href: '/assets/resume.pdf',
-      text: 'Resume',
-      primary: true,
-      Icon: ArrowDownTrayIcon,
-    },
     {
       href: `#${SectionId.Contact}`,
       text: 'Contact',
@@ -104,16 +98,14 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Our work reflects a deep understanding of the unique needs of each business we serve.
+   We incorporate functional design, aesthetic appeal, and robust construction to create furniture pieces that elevate the
+    ambiance of commercial spaces, thereby enhancing customer experiences.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'Bucharest, RO', Icon: MapIcon},
+    {label: 'Nationality', text: 'Romanians', Icon: FlagIcon},
+    {label: 'Interests', text: 'Furniture', Icon: SparklesIcon},
+    {label: 'Employment', text: 'Markap BS, inc.', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -122,69 +114,69 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
+    name: 'Quality of the finishes',
     skills: [
       {
-        name: 'English',
+        name: 'Placi',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'Gauri',
+        level: 8.4,
       },
       {
-        name: 'Spanish',
+        name: 'Alte alea',
         level: 3,
       },
     ],
   },
   {
-    name: 'Frontend development',
+    name: 'Montaje',
     skills: [
       {
-        name: 'React',
-        level: 9,
-      },
-      {
-        name: 'Typescript',
-        level: 7,
-      },
-      {
-        name: 'GraphQL',
+        name: 'Cu Dorel',
         level: 6,
+      },
+      {
+        name: 'Cu Cornel',
+        level: 9.5,
+      },
+      {
+        name: 'Cu Cristi',
+        level: 8,
       },
     ],
   },
   {
-    name: 'Backend development',
+    name: 'Proiectare',
     skills: [
       {
-        name: 'Node.js',
+        name: 'Face Parsu',
         level: 8,
       },
       {
-        name: 'Rust',
+        name: 'State',
         level: 5,
       },
       {
-        name: 'Golang',
+        name: 'Nimic',
         level: 4,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: 'Exemplu',
     skills: [
       {
-        name: 'React Native',
+        name: 'Exempu',
         level: 9,
       },
       {
-        name: 'Flutter',
+        name: 'Spati Liber',
         level: 4,
       },
       {
-        name: 'Swift',
+        name: 'SpatiuLiber2',
         level: 3,
       },
     ],
@@ -198,67 +190,67 @@ export const portfolioItems: PortfolioItem[] = [
   {
     title: 'Project title 1',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: '',
     image: porfolioImage1,
   },
   {
     title: 'Project title 2',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: '',
     image: porfolioImage2,
   },
   {
     title: 'Project title 3',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: '',
     image: porfolioImage3,
   },
   {
     title: 'Project title 4',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage4,
   },
   {
     title: 'Project title 5',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage5,
   },
   {
     title: 'Project title 6',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage6,
   },
   {
     title: 'Project title 7',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage7,
   },
   {
     title: 'Project title 8',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage8,
   },
   {
     title: 'Project title 9',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage9,
   },
   {
     title: 'Project title 10',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage10,
   },
   {
     title: 'Project title 11',
     description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    url: ' ',
     image: porfolioImage11,
   },
 ];
@@ -266,26 +258,11 @@ export const portfolioItems: PortfolioItem[] = [
 /**
  * Resume section -- TODO: Standardize resume contact format or offer MDX
  */
-export const education: TimelineItem[] = [
-  {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-  {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
-  },
-];
-
 export const experience: TimelineItem[] = [
   {
     date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    location: 'La Auchan la munca',
+    title: 'Facem dregem Auchan',
     content: (
       <p>
         Describe work, special projects, notable achievements, what technologies you have been working with, and
@@ -295,8 +272,8 @@ export const experience: TimelineItem[] = [
   },
   {
     date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    location: 'La Arad cu Cristi in delegatie',
+    title: 'LeroyMerlin magazine',
     content: (
       <p>
         Describe work, special projects, notable achievements, what technologies you have been working with, and
@@ -313,18 +290,18 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      name: 'Nicu de la gaze',
+      text: 'Imi place de ei sunt baieti harnici',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      name: 'Gogu de la CNC',
+      text: 'Super de treaba si nice!',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      name: 'Marius de la Auchan',
+      text: 'Mi-a placut cum au uns picioarele de la stand-uri cu slanina!',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
     },
   ],
@@ -336,27 +313,22 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: "Are you interested in our high-quality, modern furniture for your business space? Do you have questions, or do you need further information? We're here to help",
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: 'contact@markapbs.ro',
+      href: 'mailto:contact@markapbs.ro',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
+      text: 'Bucuresti Soseaua Berceni 104',
       href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
-    },
-    {
-      type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: '@markap',
+      href: 'https://www.instagram.com/markap/',
     },
   ],
 };
@@ -365,9 +337,7 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/tbakerx/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/markap/'},
+  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/markap/'},
+  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/markap'},
 ];
